bd92a16e2adf381b60b5a4e376ece90d5233aabd
Hamit Burak Emre <hamitburakemre@gmail.com>
2017-11-17 02:59:25 +0300

    Implement add, delete methods of map class

--- map/edge.py
+++ map/edge.py
@@ -1,9 +1,10 @@
 from math import sqrt
 
 class Edge(object):
-    def __init(self, start_node, end_node):
+    def __init(self, start_node, end_node, lanes_count):
         self.start = start_node
         self.end = end_node
+        self.lanes_count = lanes_count
         self.weight = self.length()
     
     def length(self):
--- map/map.py
+++ map/map.py
@@ -13,23 +13,56 @@ class Map(object):
         ''' 
         create a vertice at (x,y) with given id
         '''
+        new_node = Node(id, x, y)
+        nodes[i] = new_node
 
     def delete_node(self, id):
         ''' 
         delete the vertice with given id. All edges connecting the vertice are also deleted
         '''
+        if id not in nodes:
+            return
+        # Unlink children:
+        for child in self.children[id]:
+            del self.parent[child][id]
+
+        # Unlink parents:
+        for parent in self.parents[id]:
+            del self.child[parent][id]
         
-    def add_road(self, id1, id2, nlanes = 1, bidir = True):
+        del self.children[id]
+        del self.parents[id]
+        del self.nodes[id]
+        
+
+    def add_road(self, id1, id2, nlanes = 1, bidir = False):
         ''' 
         add an edge from id1 to id2. Number of lanes is set for
         each edge and if bidir is True another edge in opposite
         direction is also created. 
         '''
-    def delete_road(self, id1, id2, bidir = True):
+        start_node = nodes[id1]
+        end_node = nodes[id2]
+        
+        new_road = Edge(start_node, end_node, nlanes)
+
+        self.childs[id1][id2] = new_road
+        self.parents[id2][id1] = new_road
+
+        if bidir:
+            self.add_road(id2, id1, nlanes)
+        
+        
+    def delete_road(self, id1, id2, bidir = False):
         ''' 
         delete the edge between id1 and id2. If bidir is True,
         the edge in opposite direction is also deleted.
         '''
+        del self.children[id1][id2]
+        del self.parents[id2][id1]
+
+        if bidir:
+            self.delete_road(id2, id1)
 
     def get_shortest_path(self,id1,id2):
         ''' 
