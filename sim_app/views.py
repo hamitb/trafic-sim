4e82b2cb8058c2617c8d1f7783c29fb1904b9331
Hamit Burak Emre <hamitburakemre@gmail.com>
2018-01-02 11:03:27 +0300

    Ckean up

--- sim_app/views.py
+++ sim_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'message': 'Hello'
+        'form_data': request.POST,
     }
+
     return render(request, 'sim_app/index.html', context)
